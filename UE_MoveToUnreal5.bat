01183fec38da765dff94d1cfa56e4701badbcbea
Bionic Ape <51401866+BionicApe@users.noreply.github.com>
2021-05-28 18:23:34 +0200

    Progress

---
+++ UE_MoveToUnreal5.bat
@@ -0,0 +1,4 @@
+B:/GamesEpic/UE_5.0EA/Engine/Binaries\DotNET/UnrealBuildTool/UnrealBuildTool.exe  -projectfiles -project="A:/Dis/UE4ThirdPartyExample/ThirPartyExample.uproject" -game -rocket -progress
+
+@echo off
+set /p DUMMY=Hit ENTER to continue...
